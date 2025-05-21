c233929f138d1bbc6d6792fc4995d7a41bc1b313
amirreza <amir.rsf1380@gmail.com>
2025-05-21 17:20:25 +0330

    comment out pokemon thing

--- Configs/.user.zsh
+++ Configs/.user.zsh
@@ -2,11 +2,11 @@
 # Commands to execute on startup (before the prompt is shown)
 # This is a good place to load graphic/ascii art, display system information, etc.
 
-if command -v pokego >/dev/null; then
-    pokego --no-title -r 1,3,6
-elif command -v pokemon-colorscripts >/dev/null; then
-    pokemon-colorscripts --no-title -r 1,3,6
-elif command -v fastfetch >/dev/null; then
+#if command -v pokego >/dev/null; then
+#    pokego --no-title -r 1,3,6
+#elif command -v pokemon-colorscripts >/dev/null; then
+#    pokemon-colorscripts --no-title -r 1,3,6
+if command -v fastfetch >/dev/null; then
     fastfetch --logo-type kitty
 fi
 
