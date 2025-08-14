5818a2fdfcad6c5a66cbeaf521562d3436ba90df
amirreza <amir.rsf1380@gmail.com>
2025-08-14 18:04:30 +0330

    dotnet plugin

--- Configs/.config/zsh/user.zsh
+++ Configs/.config/zsh/user.zsh
@@ -1,18 +1,18 @@
 #  Startup 
 # Commands to execute on startup (before the prompt is shown)
 # Check if the interactive shell option is set
-if [[ $- == *i* ]]; then
+# if [[ $- == *i* ]]; then
 #    # This is a good place to load graphic/ascii art, display system information, etc.
 #    if command -v pokego >/dev/null; then
 #        pokego --no-title -r 1,3,6
 #    elif command -v pokemon-colorscripts >/dev/null; then
 #        pokemon-colorscripts --no-title -r 1,3,6
-    if command -v fastfetch >/dev/null; then
-        if do_render "image"; then
-            fastfetch --logo-type kitty
-        fi
-    fi
-fi
+#     if command -v fastfetch >/dev/null; then
+#         if do_render "image"; then
+#             fastfetch --logo-type kitty
+#         fi
+#     fi
+# fi
 
 #   Overrides 
 # HYDE_ZSH_NO_PLUGINS=1 # Set to 1 to disable loading of oh-my-zsh plugins, useful if you want to use your zsh plugins system
@@ -26,5 +26,6 @@ if [[ ${HYDE_ZSH_NO_PLUGINS} != "1" ]]; then
     plugins=(
         "sudo"
         direnv
+        dotnet
     )
 fi
